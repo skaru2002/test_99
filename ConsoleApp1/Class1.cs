cfc2432d1d25073037d8c0c573a8f4aec752accf
skaru2002 <skaru2002@naver.com>
2021-01-28 21:00:45 +0900

    초기 커밋.

---
+++ ConsoleApp1/Class1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace ConsoleApp1
+{
+    class Class1
+    {
+
+        static void Main(string[] args)
+        {
+            var isRunning = true;
+            var isPaused = true;
+
+            while (true)
+            {
+                if (isRunning == false) break;
+
+                while (isPaused)
+                {
+                    Thread.Sleep(0);
+
+                }
+
+                Play();
+
+                Thread.Sleep(0);
+            }
+        }
+
+        private static void Play()
+        {
+
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Threading;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 2, j = 1, n =0;
+
+            //for (i = 2; i <= 9; i++)
+            //{
+            //    Console.WriteLine("{0}단\n", i);
+            //    for (j = 1; j <= 9; j++)
+            //    {
+            //        n = i * j;
+            //        Console.WriteLine("{0}*{1}={2}", i, j, n);
+            //    }
+            //}
+            //Console.WriteLine("Hello World!");
+
+            //while (i<10)
+            //{
+            //    Console.WriteLine("\n{0}단", i);
+            //    while (j<10)
+            //    {
+            //        Console.WriteLine("{0}*{1}={2}", i, j, i * j);
+            //        j++;
+            //    }
+            //    i++;
+            //    j = 1;
+            //}
+
+            //while (i<10)
+            //{
+            //    if (j==1)
+            //    {
+            //        Console.WriteLine("\n{0}단", i);
+            //    }
+
+            //    Console.WriteLine("{0}*{1}={2}", i, j, i * j);
+            //    j++;
+
+            //    if (j == 10)
+            //    {
+            //        i++;
+            //        j = 1;
+            //    }
+            //}
+
+            //for (i = 2; i <= 9; i++)
+            //{
+            //    Console.WriteLine("{0}단\n", i);
+            //    for (j = 1; j <= 9; j++)
+            //    {
+            //        n = i * j;
+            //        Console.WriteLine("{0}*{1}={2}", i, j, n);
+            //    }
+            //}
+            //Console.WriteLine("Hello World!");
+
+            
+            while (i+j > 18)
+            {
+                Console.WriteLine("{0}단\n", i);
+                Console.WriteLine("{0}*{1}={2}", i, j, i * j);
+                j++;
+
+                break;
+
+
+            }
+
+        }
+
+        
+    }
+}
